1ae2078a7d120bbf17d5bccd60149ba605bf64b8
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-10-19 02:51:00 -0700

    Print percent

---
+++ print_percent.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_percent - prints percent
+ * @args: arguments
+ * Return: 0
+ */
+int print_percent(va_list args)
+{
+	(void)args;
+	_putchar('%');
+	return (1);
+}
