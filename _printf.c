65e1e7c47ccf2b452364a50ce97af21b338e5e26
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-10-19 06:24:11 -0700

    printf implementation

--- _printf.c
+++ _printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
-	for (i = 0; format && format[i] != '\0'; i++, count++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
