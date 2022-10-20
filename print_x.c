da2d5ab31e4a383641f4710642057b827718b493
mukiliderrick <derrickmukili64@gmail.com>
2022-10-20 15:52:02 -0700

    betty style

--- print_x.c
+++ print_x.c
@@ -3,15 +3,15 @@
 /**
  * print_x - prints the number to base 16 in lowercase
  * @args: the argumets
+ * @valist - 1st argument
  * Return: number count
 */
-int print_x(va_list args)
+int print_x(va_list, args)
 {
-    int i = 0;
-    int count = 0;
+	int i = 0;
+	int count = 0;
 
-    i = va_arg(args, unsigned int);
-    count = base_convert(i,16);
-
-    return (count);
+	i = va_arg(args, unsigned int);
+	count = base_convert(i, 16);
+	return (count);
 }
