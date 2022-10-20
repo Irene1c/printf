642d9948c7fd14e85c66e7ef4d727f0d7b90cf6f
mukiliderrick <derrickmukili64@gmail.com>
2022-10-20 22:16:02 +0300

    prints number to the base of 2

---
+++ print_b.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_b - prints number to base of 2
+ * @args: the arguments
+ * Return: number count
+*/
+int print_b(va_list args)
+{
+    int i = 0;
+    int count = 0;
+
+    i = va_arg(args unsigned int);
+    count = base_convert(i, 2);
+
+    return (count);
+}
