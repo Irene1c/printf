8910ad2e09359f3aaa3d2e7c4ba579ba52cae8ab
mukiliderrick <derrickmukili64@gmail.com>
2022-10-20 15:13:05 -0700

    base convert

--- base_convert.c
+++ base_convert.c
@@ -8,32 +8,30 @@
 */
 int base_convert(unsigned int n, int base)
 {
-    char rep1[] = "123456789ABCDEF";
-    char rep2[] = "123456789abcdef";
-    char array[100];
-    char *ptr;
-    int i = 99;
-    int p = 0;
-
-    ptr = &array[99];
-    *ptr = '\0';
-
-    while (n != 0)
-    {
-        if (n != 0)
-        {
-            *--ptr = rep1[n % base];
-        }
-        else
-        {
-            *--ptr = rep2[n % base];
-        }
-        n = n / base;
-        i--;
-    }
-    for (p = i; array[p] != 0; p++)
-    {
-        _putchar(array[p]);
-    }
-    return (99 - i);
+	char rep1[] = "123456789ABCDEF";
+	char rep2[] = "123456789abcdef";
+	char array[100];
+	char *ptr;
+	int i = 99;
+	int p = 0;
+	ptr = &array[99];
+	*ptr = '\0';
+	while (n != 0)
+	{
+		if (n != 0)
+		{
+			*--ptr = rep1[n % base];
+		}
+		else
+		{
+			*--ptr = rep2[n % base];
+		}
+		n = n / base;
+		i--;
+	}
+	for (p = 1; array[p] != 0; p++)
+	{
+		_putchar(array[p]);
+	}
+	return (99 - i);
 }
