cff7333585e93531c66e3543fed2679d3e544aaa
mukiliderrick <derrickmukili64@gmail.com>
2022-10-20 15:40:19 -0700

    betty style

--- print_u.c
+++ print_u.c
@@ -7,11 +7,10 @@
 */
 int print_u(va_list args)
 {
-    int i = 0;
-    int count = 0;
+	int i = 0;
+	int count = 0;
 
-    i = va_arg(args unsigned int);
-    count = base_convert(i, 10);
-
-    return (count);
+	i = va_arg(args unsigned, int);
+	count = base_convert(i, 10);
+	return (count);
 }
