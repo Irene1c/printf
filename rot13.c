6326fc666f0b775976e423f76bd70386c8a91f87
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-10-22 10:01:15 -0700

    string encoded with rot13

--- rot13.c
+++ rot13.c
@@ -11,24 +11,27 @@ int print_R(va_list args)
 	char *str;
 	int i = 0;
 	int a;
+	int count = 0;
 	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	str = va_arg(args, char *);
 	for (; str[i] != '\0'; i++)
 	{
-		for (a = 0; a <= 52; a++)
+		for (a = 0; a < 52; a++)
 		{
 			if (str[i] == s[a])
 			{
+				count++;
 				_putchar(rot13[a]);
 				break;
 			}
-		}
-		if (s[a + 1] == 0)
-		{
-			_putchar(str[i]);
+			else if (s[a + 1] == 0)
+			{
+				_putchar(str[i]);
+				count++;
+			}
 		}
 	}
-	return (i);
+	return (count);
 }
